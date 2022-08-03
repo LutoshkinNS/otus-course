c1670a18cfcfdd747dd19f39f5a0334e496abfe8
Nikita <n28280202@gmail.com>
2022-08-03 19:55:50 +0300

    add App testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,27 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import '@testing-library/jest-dom';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 
 describe('App tests', () => {
   let ToggleButton: HTMLButtonElement;
-  let DateList: Promise<HTMLUListElement>;
   beforeEach(() => {
     render(<App />);
     ToggleButton = screen.getByTestId('ToggleButtonTest');
-    DateList = screen.findByTestId('DateListTest');
   });
 
   it('should render toggle button', () => {
     expect(ToggleButton).toBeInTheDocument();
+    expect(screen.queryByTestId('DateListTest')).not.toBeInTheDocument();
     expect(ToggleButton).toHaveTextContent('show');
   });
 
   it('should show list after click ToggleButton', async () => {
-    await userEvent.click(ToggleButton);
-    await waitFor(() => expect(DateList).toBeInTheDocument(), { timeout: 5000 });
+    /*не смог понять почему не срабатывает screen.getByTestId('ToggleButtonTest') */
+    expect(ToggleButton).toHaveTextContent('show');
+    await userEvent.click(screen.getByTestId('ButtonTest'));
+    expect(ToggleButton).toHaveTextContent('hide');
+    expect(screen.getByTestId('DateListTest')).toBeInTheDocument();
+    expect(screen.getByTestId('DateListTest').innerHTML.length).toBeGreaterThan(28);
   });
 });

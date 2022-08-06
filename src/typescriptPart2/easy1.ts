4f8ddf97bc1b97b16c8c5c7a49fcac7a3b7bd10e
Nikita <n28280202@gmail.com>
2022-08-06 11:25:19 +0300

    fix easy1

---
+++ src/typescriptPart2/easy1.ts
@@ -0,0 +1,19 @@
+// Задание первого уровня 1
+// В функцию приходит массив состояний заказа и фильтруется
+// Нужно заменить FIXME на тип который вычисляется на основе OrderState
+
+type FIXME = any;
+
+const orderStates = ['initial', 'inWork', 'buyingSupplies', 'producing', 'fullfilled'] as const;
+
+type OrderState = typeof orderStates[number];
+
+export const getUserOrderStates = (orderStates: OrderState[]): string[] => {
+  const filteredStates = [] as string[];
+  orderStates.forEach((element) => {
+    if (element !== 'buyingSupplies' && element !== 'producing') {
+      filteredStates.push(element);
+    }
+  });
+  return filteredStates;
+};
